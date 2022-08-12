b292da8bc7f79d20be82ef2a77a476d663546683
Linah Muhonja <linahmuhonjaimbari@gmail.com>
2022-08-12 19:05:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector('main').remove('main');
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1 id='victory'>YOUR-NAME is the champion </h1>";
